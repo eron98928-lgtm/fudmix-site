1affa34604d378c9420627df2ee0a6479bb2cf0f
eron98928-lgtm <eron98928@gmail.com>
2026-05-20 15:43:18 -0300

    Update client.ts

--- fudmix-site/src/integrations/supabase/client.ts
+++ fudmix-site/src/integrations/supabase/client.ts
@@ -2,9 +2,9 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = "https://fgridsakklpswtvkoqld.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_c54Wl9MKPtvAnHzPaK4FBg_XVBUcBuLBMDUANDqb3Y";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZncmlkc2Fra2xwc3d0dmtvcWxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwMjgyODEsImV4cCI6MjA5NDYwNDI4MX0.ugmk2VRDDG7wjHGk42veHMjaRMNVyOCAnfFkeZfB_o0";
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     storage: typeof window !== 'undefined' ? localStorage : undefined,
     persistSession: true,
